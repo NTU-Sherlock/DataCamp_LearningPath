c64e68fe3e01f975d58893572dd3d9ed5e367cfd
Shiuan <kyle5275@gmail.com>
2020-05-23 14:59:41 +0800

    Create C1_cohort.py

---
+++ customer_segmentation/C1_cohort.py
@@ -0,0 +1,97 @@
+# Define a function that will parse the date
+def get_day(x): return dt.datetime(x.year, x.month, x.day) 
+
+# Create InvoiceDay column
+online['InvoiceDay'] = online['InvoiceDate'].apply(get_day) 
+
+# Group by CustomerID and select the InvoiceDay value
+grouping = online.groupby('CustomerID')['InvoiceDay'] 
+
+# Assign a minimum InvoiceDay value to the dataset
+online['CohortDay'] = grouping.transform(min)
+
+# View the top 5 rows
+print(online.head())
+try
+
+
+def get_date_int(df, column):
+    year = df[column].dt.year
+    month = df[column].dt.month
+    day = df[column].dt.day
+    return year, month, day
+
+# Get the integers for date parts from the `InvoiceDay` column
+invoice_year, invoice_month, invoice_day = get_date_int(online, 'InvoiceDay')
+
+# Get the integers for date parts from the `CohortDay` column
+cohort_year, cohort_month, cohort_day = get_date_int(online, 'CohortDay')
+
+
+# Calculate difference in years
+years_diff = invoice_year - cohort_year
+
+# Calculate difference in months
+months_diff = invoice_month - cohort_month
+
+# Calculate difference in days
+days_diff = invoice_day - cohort_day
+
+# Extract the difference in days from all previous values
+online['CohortIndex'] = years_diff * 365 + months_diff * 30 + days_diff  + 1
+print(online.head())
+
+
+# Count the number of unique values per customer ID
+cohort_data = grouping['CustomerID'].apply(pd.Series.nunique).reset_index()
+
+# Create a pivot 
+cohort_counts = cohort_data.pivot(index='CohortMonth', columns='CohortIndex', values='CustomerID')
+
+
+# Select the first column and store it to cohort_sizes
+cohort_sizes = cohort_counts.iloc[:,0]
+
+# Divide the cohort count by cohort sizes along the rows
+retention = cohort_counts.div(cohort_sizes, axis=0)
+
+
+# Create a groupby object and pass the monthly cohort and cohort index as a list
+grouping = online.groupby(['CohortMonth','CohortIndex']) 
+
+# Calculate the average of the unit price column
+cohort_data = grouping['UnitPrice'].mean()
+
+# Reset the index of cohort_data
+cohort_data = cohort_data.reset_index()
+
+# Create a pivot 
+average_price = cohort_data.pivot(index='CohortMonth', columns='CohortIndex', values='UnitPrice')
+print(average_price.round(1))
+
+
+#Visualize with Seaborn
+# Import seaborn package as sns
+import seaborn as sns
+
+# Initialize an 8 by 6 inches plot figure
+plt.figure(figsize=(8, 6))
+
+# Add a title
+plt.title('Average Spend by Monthly Cohorts')
+
+# Create the heatmap
+sns.heatmap(average_quantity, annot= True, cmap='Blues')
+plt.show()
+
+
+
+# Initialize a plot with a figure size of 8 by 2 inches 
+plt.figure(figsize=(8, 2))
+
+# Add the plot title
+plt.title('Relative importance of attributes')
+
+# Plot the heatmap
+sns.heatmap(data=relative_imp, annot=True, fmt='.2f', cmap='RdYlGn')
+plt.show()
