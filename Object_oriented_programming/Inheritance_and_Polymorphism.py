44b90b48253a2097dc64d97e6ec41f6d760d43c1
NTU_Sherlock <kyle5275@gmail.com>
2021-01-30 16:31:05 +0800

    commit

--- Object_oriented_programming/Inheritance_and_Polymorphism.py
+++ Object_oriented_programming/Inheritance_and_Polymorphism.py
@@ -23,22 +23,97 @@ class Player:
         self.position = 0
     # Add a move() method with steps parameter
     def move(self, steps):
-        Player.MAX_POSITION
         if self.position + steps < Player.MAX_POSITION:
             self.position+=steps
         else:
             self.position = Player.MAX_POSITION
+    def draw(self):
+        drawing = "-" * self.position + "|" +"-"*(Player.MAX_POSITION - self.position)
+        print(drawing)
 
 
     
 
        
-    # This method provides a rudimentary visualization in the console    
-    def draw(self):
-        drawing = "-" * self.position + "|" +"-"*(Player.MAX_POSITION - self.position)
-        print(drawing)
+# This method provides a rudimentary visualization in the console    
 
 p = Player(); p.draw()
 p.move(4); p.draw()
 p.move(5); p.draw()
 p.move(3); p.draw()
+
+
+# Changing class attributes
+# Create Players p1 and p2
+p1, p2 = Player(), Player()
+
+print("MAX_SPEED of p1 and p2 before assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+# ---MODIFY THIS LINE--- 
+Player.MAX_SPEED = 7
+
+print("MAX_SPEED of p1 and p2 after assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+print("MAX_SPEED of Player:")
+# Print Player.MAX_SPEED
+print(Player.MAX_SPEED)
+
+
+
+# import datetime from datetime
+from datetime import datetime
+
+class BetterDate:
+    def __init__(self, year, month, day):
+      self.year, self.month, self.day = year, month, day
+      
+    @classmethod#Use Decorator
+    def from_str(cls, datestr):
+        year, month, day = map(int, datestr.split("-"))
+        return cls(year, month, day)
+    
+    # Define a class method from_datetime accepting a datetime object
+    @classmethod#Use Decorator
+    def from_datetime(cls, datetime):
+        year, month, day = datetime.year, datetime.month, datetime.day
+        return cls(year, month, day)
+
+
+
+
+# You should be able to run the code below with no errors: 
+today = datetime.today()     
+bd = BetterDate.from_datetime(today)   
+print(bd.year)
+print(bd.month)
+print(bd.day)
+
+#Create a subclass and learn inheritance
+class Employee:
+  MIN_SALARY = 30000    
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+  def give_raise(self, amount):
+    self.salary += amount      
+        
+# MODIFY Manager class and add a display method
+class Manager(Employee):
+  def display(self):
+    print('Manager' + self.name)
+
+mng = Manager("Debbie Lashko", 86500)
+print(mng.name)
+
+# Call mng.display()
+mng.display()
